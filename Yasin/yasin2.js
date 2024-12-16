a3b1c74ea0739ee10b05632645eb5b1635d9fd95
Yasinayn <Yasin558ayd558@gmail.com>
2024-12-16 21:31:27 +0300

    Add files via upload

---
+++ Yasin/yasin2.js
@@ -0,0 +1,12 @@
+function Buton1(){
+document.write("Adım:Yasin <br> Soyismim:Aydın <br> Doğum Yerim:İstanbul/Zeytinburnu ")
+document.write("<br> Doğum Tarihim:02.06.2010 <br>")
+}
+function Buton2(){
+document.write("İlkokul: Karatay İlkokulu <br> Ortaokul:Atatürk Ortaokulu <br> Lise: Abdulhamid Han Anadolu Lisesi (Devam Ediyor)")
+
+}
+function Buton3(){
+document.write("Bisiklet Sürmek <br> Bilgisayarı Karıştırmak <br> Top Oynamak")
+
+}
